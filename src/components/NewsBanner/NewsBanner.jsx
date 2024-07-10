da94ac54553b0761268c37ac2f0f8e2315ea8839
Ira Ryzha <ira.ryzha2803@gmail.com>
2024-07-10 10:40:55 +0300

    add search and debounce

--- src/components/NewsBanner/NewsBanner.jsx
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -6,9 +6,9 @@ function NewsBanner({ item }) {
   return (
     <div className={styles.banner}>
       <Image image={item?.image} />
-      <h3 className={styles.title}>{item.title}</h3>
+      <h3 className={styles.title}>{item?.title}</h3>
       <p className={styles.extra}>
-        {formatTimeAgo(item.published)} by {item.author}
+        {formatTimeAgo(item?.published)} by {item?.author}
       </p>
     </div>
   );
